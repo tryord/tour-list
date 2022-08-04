b75823eed1c7f7721b0ab319465ac8c01859b2ef
Akash <akashharikumar2013@gmail.com>
2022-08-04 13:46:08 +0530

    Finished the beginner project: Tour List

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import Loading from './Loading';
+import Tours from './Tours';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const url = "https://course-api.com/react-tours-project"
+  const [tours,setTours] = useState([]);
+  const [loading,setLoading] = useState(true)
+
+  const fetchTours = async ()=>{
+    setLoading(true)
+    try{
+      const response = await fetch(url);
+      const toursResponse = await response.json();
+      setLoading(false)
+      setTours(toursResponse)
+      console.log(tours)
+    }catch(e){
+      setLoading(false)
+      console.log("Error while fetching: "+ e)
+    }
+  }
+  useEffect(()=>{
+    fetchTours();
+  },[])
+
+  const handleDelete = (id)=>{
+    const newTours = tours.filter((tour)=>tour.id!==id)
+    setTours(newTours)
+  }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="Heading">Our Tours</h1>
+      <div className="underline"></div>
+     {loading && <Loading/>}
+     {!loading && <Tours tours={tours} handleDelete={handleDelete}/>}
     </div>
   );
 }
